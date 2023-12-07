ada4b770b694e641a9b28a66ddea43c33943b0b1
GuilhermeSouzaViana <vianagui7744@gmail.com>
2023-12-07 17:10:00 -0300

    feat

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,15 +1,21 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
-import { PrincipalComponent } from "./principal/principal.component"; //importado aqui
+import { PrincipalComponent } from "./principal/principal.component"; 
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+
 
 @Component({
-    selector: 'app-root',
-    standalone: true,
-    templateUrl: './app.component.html',
-    styleUrl: './app.component.css',
-    imports: [CommonModule, RouterOutlet, PrincipalComponent] // referenciado aqui
+  selector: 'app-root',
+  standalone: true,
+  imports: [CommonModule, RouterOutlet, PrincipalComponent, FormsModule,HttpClientModule],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.css'
 })
+
+
+
 export class AppComponent {
   title = 'api-front';
 }
--- src/app/principal/principal.component.ts
+++ src/app/principal/principal.component.ts
@@ -1,12 +1,30 @@
 import { Component } from '@angular/core';
+import { Cliente } from '../modelo/Cliente';
+import { ClienteService } from '../servico/cliente.service';
 
 @Component({
   selector: 'app-principal',
   standalone: true,
   imports: [],
   templateUrl: './principal.component.html',
-  styleUrl: './principal.component.css'
+  styleUrls: ['./principal.component.css']
 })
+
 export class PrincipalComponent {
+  btnCadastro: boolean = true;
+
+
+  clientes: Cliente[] = [];
+
+  constructor(private servico: ClienteService) { }
+
+
+  selecionar(): void {
+    this.servico.selecionar()
+      .subscribe(retorno => this.clientes = retorno);
+
+  }
+
+
 
 }
